e86987ab42b7009ada34268fa25617b38fc87170
Thanapoom Rattanathumawat <thanapoom.ta@ku.th>
2018-04-16 18:55:06 +0700

    Added Terrain/Monster.blend

--- The Maze Game/src/mygame/Main.java
+++ The Maze Game/src/mygame/Main.java
@@ -1,10 +1,13 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
+import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 
 /**
@@ -21,14 +24,15 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
+        
+        // Set location of cam to be higher than terrain ground
+        cam.setLocation(new Vector3f(0,1,0));
+        
+        // Adding the terrain and it's texture
+        Spatial scene = assetManager.loadModel("Scenes/Maze Terrain.j3o");
+        
+        rootNode.attachChild(scene);
+        
     }
 
     @Override
