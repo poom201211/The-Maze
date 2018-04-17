7694a83a9e95e3f1190a5cb1805c39fa6700b651
poonnanun <poonnanun1@gmail.com>
2018-04-17 22:23:34 +0700

    Added walkable character

--- The Maze Game/src/mygame/Main.java
+++ The Maze Game/src/mygame/Main.java
@@ -1,22 +1,36 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.light.DirectionalLight;
-import com.jme3.material.Material;
-import com.jme3.math.ColorRGBA;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.collision.shapes.CapsuleCollisionShape;
+import com.jme3.bullet.collision.shapes.CollisionShape;
+import com.jme3.bullet.control.CharacterControl;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.bullet.util.CollisionShapeFactory;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
-import com.jme3.scene.Geometry;
 import com.jme3.scene.Spatial;
-import com.jme3.scene.shape.Box;
 
 /**
  * This is the Main Class of your Game. You should only do initialization here.
  * Move your Logic into AppStates or Controls
  * @author normenhansen
  */
-public class Main extends SimpleApplication {
+public class Main extends SimpleApplication implements ActionListener{
 
+    private BulletAppState bulletAppState;
+    private Spatial scene;
+    private RigidBodyControl landscape;
+    private CharacterControl player;
+    private Vector3f walkDirection = new Vector3f();
+    private boolean left = false, right = false, up = false, down = false;
+    
+    private Vector3f camDir = new Vector3f();
+    private Vector3f camLeft = new Vector3f();
+    
     public static void main(String[] args) {
         Main app = new Main();
         app.start();
@@ -25,23 +39,85 @@ public class Main extends SimpleApplication {
     @Override
     public void simpleInitApp() {
         
-        // Set location of cam to be higher than terrain ground
-        cam.setLocation(new Vector3f(0,1,0));
+        bulletAppState = new BulletAppState();
+        stateManager.attach(bulletAppState);
         
-        // Adding the terrain and it's texture
-        Spatial scene = assetManager.loadModel("Scenes/Maze Terrain.j3o");
+        keySet();
+        
+        scene = assetManager.loadModel("Scenes/MazeTerrian.j3o");
+        
+        CollisionShape sceneShape = CollisionShapeFactory.createMeshShape(scene);
+        landscape = new RigidBodyControl(sceneShape, 0);
+        scene.addControl(landscape);
+        
+        CapsuleCollisionShape capsuleShape = new CapsuleCollisionShape(1.5f, 6f, 1);
+        player = new CharacterControl(capsuleShape, 0.05f);
+        player.setJumpSpeed(20);
+        player.setFallSpeed(30);
+        player.setGravity(new Vector3f(0,-30f,0));
+        player.setPhysicsLocation(new Vector3f(0, 10, 0));
         
         rootNode.attachChild(scene);
-        
+        bulletAppState.getPhysicsSpace().add(landscape);
+        bulletAppState.getPhysicsSpace().add(player);
     }
 
     @Override
     public void simpleUpdate(float tpf) {
-        //TODO: add update code
+        camDir.set(cam.getDirection()).multLocal(0.6f);
+        camLeft.set(cam.getLeft()).multLocal(0.4f);
+        walkDirection.set(0, 0, 0);
+        if (left) {
+            walkDirection.addLocal(camLeft);
+        }
+        if (right) {
+            walkDirection.addLocal(camLeft.negate());
+        }
+        if (up) {
+            walkDirection.addLocal(camDir);
+        }
+        if (down) {
+            walkDirection.addLocal(camDir.negate());
+        }
+        player.setWalkDirection(walkDirection);
+        cam.setLocation(player.getPhysicsLocation());
     }
 
     @Override
     public void simpleRender(RenderManager rm) {
         //TODO: add render code
+    }
+
+    public void keySet(){
+        inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_A));
+        inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_D));
+        inputManager.addMapping("Up", new KeyTrigger(KeyInput.KEY_W));
+        inputManager.addMapping("Down", new KeyTrigger(KeyInput.KEY_S));
+        inputManager.addMapping("Jump", new KeyTrigger(KeyInput.KEY_SPACE));
+        
+        inputManager.addListener(this, "Left");
+        inputManager.addListener(this, "Right");
+        inputManager.addListener(this, "Up");
+        inputManager.addListener(this, "Down");
+        inputManager.addListener(this, "Jump");
+        
+    }
+    
+    @Override
+    public void onAction(String name, boolean isPressed, float tpf) {
+        switch(name){
+            case "Left": left = isPressed; break;
+            
+            case "Right": right = isPressed; break;
+            
+            case "Up": up = isPressed; break;
+            
+            case "Down": down = isPressed; break;
+            
+            case "Jump": player.jump(new Vector3f(0,15f,0)); break;
+            
+            default: break;
+            
+        }
     }
 }
